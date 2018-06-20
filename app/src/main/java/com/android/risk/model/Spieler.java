c600f77b03043d73f43211ca0e9e72b6415c3d70
jürgen.maurer <jurgenm00@gmail.com>
2018-06-20 09:20:22 +0200

    Sachen

--- app/src/main/java/com/android/risk/model/Spieler.java
+++ app/src/main/java/com/android/risk/model/Spieler.java
@@ -100,31 +100,31 @@ public class Spieler {
     }
 
     /**
-     * Beendet die dritte Phase
+     * Beendet die dritte Phase.
      */
     public void zugBeenden() {
         spiel.naechstePhase();
     }
 
     /**
-     * Setzt das Attribut amZug
-     * @param amZug
+     * Setzt das Attribut amZug.
+     * @param amZug Der neue Inhalt der Variable amZug
      */
     public void setAmZug(boolean amZug) {
         this.amZug = amZug;
     }
 
     /**
-     * Gibt eine Liste der besetzten Regionen eines Spielers zurück
-     * @return
+     * Gibt eine Liste der besetzten Regionen eines Spielers zurück.
+     * @return Die Liste der besetzten Regionen
      */
     public ArrayList<Region> gibBesetzteRegionen() {
         return besetzteRegionen;
     }
 
     /**
-     * Gibt die Anzahl der besetzten Regionen zurück
-     * @return
+     * Gibt die Anzahl der besetzten Regionen zurück.
+     * @return Die Anzahl der besetzten Regionen
      */
     public int gibAnzahlBesetzteRegionen() {
         return besetzteRegionen.size();
